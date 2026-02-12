f7ec9b34315f50f612a4d8972c9030e03161cb54
haikali3 <manfdvcl9@gmail.com>
2026-02-12 09:46:56 +0800

    fix: reorder writeHeader call in writeJSON function for proper response handling

--- internal/handlers/helper.go
+++ internal/handlers/helper.go
@@ -57,7 +57,7 @@ func Make(h APIFunc) http.HandlerFunc {
 }
 
 func writeJSON(w http.ResponseWriter, status int, v any) error {
-	w.WriteHeader(status)
 	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
 	return json.NewEncoder(w).Encode(v)
 }
